5629751557baa42ef3b2232a08e6fecbebee8f96
EliDzn <elialeandro.dizon@gmail.com>
2026-09-26 01:55:18 +0800

    Implement Test Live Site Link

--- src/components/case-studies/case-study-live-site.tsx
+++ src/components/case-studies/case-study-live-site.tsx
@@ -7,16 +7,14 @@ type LiveSiteProps = {
 export default function CaseStudyLiveSite({ href }: LiveSiteProps) {
   return (
     <section className="col-span-full flex justify-center my-10">
-      <Button variant="Primary" className="w-full" asChild>
-        <a
-          href={href}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="w-full"
-        >
-          View Live Site
-        </a>
-      </Button>
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-full"
+      >
+        View Live Site
+      </a>
     </section>
   );
 }
