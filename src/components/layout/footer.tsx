d5100d7d3d5bf1109581772044177068c1f10f7a
EliDzn <elialeandro.dizon@gmail.com>
2026-09-26 00:55:26 +0800

    Removed twMerge from footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,11 +1,8 @@
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
 import Container from "./container";
 import Text from "../ui/typography";
 
 type FooterProps = {
   variant: "main" | "case-study";
-  isActive?: boolean;
 };
 
 const footerVariants = {
@@ -14,38 +11,30 @@ const footerVariants = {
     "flex flex-col sm:flex-row justify-between items-center py-6 border-t border-muted-foreground text-subtle-foreground"
 };
 
-export default function Footer({
-  variant = "main",
-  isActive = false
-}: FooterProps) {
+export default function Footer({ variant }: FooterProps) {
   const content = (
     <>
       <div>
-        <Text variant="fineprint">
+        <Text variant="caption">
           © {new Date().getFullYear()} Eli Aleandro M. Dizon
         </Text>
       </div>
 
       {variant !== "main" && (
-        <div
-          className={clsx(
-            "flex flex-col md:flex-row gap-6 items-center mt-2 md:mt-0",
-            "justify-end"
-          )}
-        >
-          <Text variant="fineprint">
+        <div className="data-active:text-foreground data-active:font-medium transition-colors duration-200">
+          <Text variant="caption">
             <a href="mailto:elialeandro.dizon@gmail.com">
               elialeandro.dizon@gmail.com
             </a>
           </Text>
 
-          <Text variant="fineprint">
+          <Text variant="caption">
             <a href="https://linkedin.com" target="_blank" rel="noreferrer">
               LinkedIn
             </a>
           </Text>
 
-          <Text variant="fineprint">
+          <Text variant="caption">
             <a href="https://github.com" target="_blank" rel="noreferrer">
               GitHub
             </a>
@@ -56,12 +45,7 @@ export default function Footer({
   );
 
   return (
-    <footer
-      data-active={isActive || undefined}
-      className={twMerge(
-        "data-active:text-foreground data-active:font-medium transition-colors duration-200"
-      )}
-    >
+    <footer className="data-active:text-foreground data-active:font-medium transition-colors duration-200">
       {variant === "case-study" ? (
         <Container>
           <div className={footerVariants["case-study"]}>{content}</div>
